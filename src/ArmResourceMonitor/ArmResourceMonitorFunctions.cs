7a5cb28c75da715a1cecdadbfd3127bb4671f3b0
John Downs <john.downs@microsoft.com>
2020-09-23 09:22:50 +1200

    Tidy up

--- src/ArmResourceMonitor/ArmResourceMonitorFunctions.cs
+++ src/ArmResourceMonitor/ArmResourceMonitorFunctions.cs
@@ -2,7 +2,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using System;
